5b7ae328889787d5c661c0feba2618f13845bc82
Ryan Hill <rch+git@zndx.org>
2022-11-12 20:38:43 -0700

    jdk11

--- shell.nix
+++ shell.nix
@@ -1,19 +1,21 @@
+{ jdk ? "jdk11" }:
 
 let
   sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs {};
+  pkgs = import sources.nixpkgs { inherit jdk; };
 in
 with pkgs;
 pkgs.mkShell {
     buildInputs = [
-    python39Full
-    python39Packages.PyLD
-    python39Packages.pipx
-    python39Packages.pytest
-    python39Packages.pyhocon
-    python39Packages.pyparsing
-    python39Packages.sphinx
-    python39Packages.tox
+      pkgs.${jdk}
+      python39Full
+      python39Packages.PyLD
+      python39Packages.pipx
+      python39Packages.pytest
+      python39Packages.pyhocon
+      python39Packages.pyparsing
+      python39Packages.sphinx
+      python39Packages.tox
     ];
 }
 
